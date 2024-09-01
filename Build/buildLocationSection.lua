c6a9dca44f7bda43ea1ba6cacbda44feb40bd578
Evan <evan.carlin.dugard@gmail.com>
2024-09-01 20:27:13 +0200

    Correction et amélioration du builder #1

--- Build/buildLocationSection.lua
+++ Build/buildLocationSection.lua
@@ -8,88 +8,117 @@ local teleportLocation = loadstring(game:HttpGet("https://raw.githubusercontent.
 
 --// Variables
 local localPlayer = Players.LocalPlayer
+local islandTeleportButtons = {}
 
+-- Build the fast travel section
 local buildLocationSection = function(islandsMenu)
+
+    -- Create section
     local locationSection = islandsMenu.Tab("Fast travel", "rbxassetid://80160769792985")
+
+    -- Setup the default location
     locationSection.Label("Default Location")
     locationSection.Line()
+
+    -- Hub market teleport button
     locationSection.Button("🏘️ 〃 Hub (Market)", "Teleport to the Hub market", function()
         local success, errorMessage = pcall(function()
-            localPlayer.Character.HumanoidRootPart.CFrame = teleportLocation.hubMarket
+            localPlayer.Character.PrimaryPart.CFrame = teleportLocation.hubMarket
         end)
 
         if not success then
             Flux.Notification(`An error occured ! Impossible to teleport you to the Hub market. Error: {errorMessage}. Please try again.`, "OK !")
         end
     end)
+
+    -- Hub mine teleport button
     locationSection.Button("⛏️ 〃 Hub (Mine)", "Teleport to the Hub mine", function()
         local success, errorMessage = pcall(function()
-            localPlayer.Character.HumanoidRootPart.CFrame = teleportLocation.hubMine
+            localPlayer.Character.PrimaryPart.CFrame = teleportLocation.hubMine
         end)
 
         if not success then
             Flux.Notification(`An error occured ! Impossible to teleport you to the Hub mine. Error: {errorMessage}. Please try again.`, "OK !")
         end
     end)
-    locationSection.Button("🟩 〃 Slime island", "Teleport to the Slime Island", function()
+
+    -- Slime island teleport button
+    locationSection.Button("🟩 〃 Slime island", "Teleport to the Slime island", function()
         local success, errorMessage = pcall(function()
-            localPlayer.Character.HumanoidRootPart.CFrame = teleportLocation.slimeIsland
+            localPlayer.Character.PrimaryPart.CFrame = teleportLocation.slimeIsland
         end)
 
         if not success then
             Flux.Notification(`An error occured ! Impossible to teleport you to the Slime island. Error: {errorMessage}. Please try again.`, "OK !")
         end
     end)
+
+    -- Setup the merchants location
     locationSection.Line()
     locationSection.Label("Merchants Location")
     locationSection.Line()
-    locationSection.Button("🌱 🌾 〃 Seeds / Crops merchant", "Teleport to the seeds and crops merchant", function()
+
+    -- seeds/crops merchant teleport button
+    locationSection.Button("🌱 🌾 〃 Seeds / Crops merchants", "Teleport to the seeds and crops merchants", function()
         local success, errorMessage = pcall(function()
-            localPlayer.Character.HumanoidRootPart.CFrame = teleportLocation.cropsMerchant
+            localPlayer.Character.PrimaryPart.CFrame = teleportLocation.cropsMerchant
         end)
 
         if not success then
-            Flux.Notification(`An error occured ! Impossible to teleport you to seeds and crops merchant. Error: {errorMessage}. Please try again.`, "OK !")
+            Flux.Notification(`An error occured ! Impossible to teleport you to the seeds and crops merchants. Error: {errorMessage}. Please try again.`, "OK !")
         end
     end)
-    locationSection.Button("🏗️ 🏦 〃 Blocks / Banker merchant", "Teleport to the blocks and banker merchant", function()
+
+    -- blocks/banker merchant teleport button
+    locationSection.Button("🏗️ 🏦 〃 Blocks / Banker merchant", "Teleport to the blocks and banker merchants", function()
         local success, errorMessage = pcall(function()
-            localPlayer.Character.HumanoidRootPart.CFrame = teleportLocation.blockMerchant
+            localPlayer.Character.PrimaryPart.CFrame = teleportLocation.blockMerchant
         end)
 
         if not success then
-            Flux.Notification(`An error occured ! Impossible to teleport you to blocks and banker merchant. Error: {errorMessage}. Please try again.`, "OK !")
+            Flux.Notification(`An error occured ! Impossible to teleport you to the blocks and banker merchants. Error: {errorMessage}. Please try again.`, "OK !")
         end
     end)
-    locationSection.Button("🔮 〃 totems merchant", "Teleport to totems merchant", function()
+
+    -- totems merchant teleport button
+    locationSection.Button("🔮 〃 totems merchant", "Teleport to the totems merchant", function()
         local success, errorMessage = pcall(function()
-            localPlayer.Character.HumanoidRootPart.CFrame = teleportLocation.totemsMerchant
+            localPlayer.Character.PrimaryPart.CFrame = teleportLocation.totemsMerchant
         end)
 
         if not success then
-            Flux.Notification(`An error occured ! Impossible to teleport you to totems merchant. Error: {errorMessage}. Please try again.`, "OK !")
+            Flux.Notification(`An error occured ! Impossible to teleport you to the totems merchant. Error: {errorMessage}. Please try again.`, "OK !")
         end
     end)
-    locationSection.Button("🤺 〃 adventurer merchant", "Teleport to adventurer merchant", function()
+
+    -- adventurer merchant teleport button
+    locationSection.Button("🤺 〃 adventurer merchant", "Teleport to the adventurer merchant", function()
         local success, errorMessage = pcall(function()
-            localPlayer.Character.HumanoidRootPart.CFrame = teleportLocation.adventurerMerchant
+            localPlayer.Character.PrimaryPart.CFrame = teleportLocation.adventurerMerchant
         end)
 
         if not success then
-            Flux.Notification(`An error occured ! Impossible to teleport you to adventurer merchant. Error: {errorMessage}. Please try again.`, "OK !")
+            Flux.Notification(`An error occured ! Impossible to teleport you to the adventurer merchant. Error: {errorMessage}. Please try again.`, "OK !")
         end
     end)
+
+    -- Setup player's island 
     locationSection.Line()
     locationSection.Label("Players' Island")
     locationSection.Line()
 
-    local islandTeleportButtons = {}
-
+    -- Create teleport buttons for the players' island
     local function setupIslandButton()
+        
+        -- Destroy all buttons (update)
         for _, button in islandTeleportButtons do
             button.Destroy()
         end
+
+        -- Get all connected players 
         for _, player in Players:GetPlayers() do
+
+            -- If player is the client then create button for his island
             if player.UserId == localPlayer.UserId then
                 local home = locationSection.Button(`🏝️ 〃 My Island`, `Teleport to your Island`, function ()        
                     local success, errorMessage = pcall(function()
@@ -103,13 +132,15 @@ local buildLocationSection = function(islandsMenu)
                 end)
                 table.insert(islandTeleportButtons, home)
             else
+                
+                -- Create button for other players
                 local button = locationSection.Button(`👤 〃 {player.Name}'s Island`, `Teleport to {player.Name}'s Island. The player must be on his island. If he is not, you will just be teleported to him.`, function ()        
                     local success, errorMessage = pcall(function()
                         local playerCharacter = player.Character
                         if playerCharacter.PrimaryPart == nil then
-                            error("Impossible to get the player's character")
+                            error("Impossible to get the player's character", 6)
                         end
-                        localPlayer.Character.PrimaryPart.CFrame = player.Character.PrimaryPart.CFrame
+                        localPlayer.Character.PrimaryPart.CFrame = playerCharacter.PrimaryPart.CFrame
                         task.wait(2)
                         local playerIsland = Workspace.Islands:WaitForChild(`{player.UserId}-island`)
                         if playerIsland.PrimaryPart == nil then
