2c10a1c47b36564605fe6d2eb06758988350b483
Evan <evan.carlin.dugard@gmail.com>
2024-09-10 23:50:02 +0200

    ajout du data manager

---
+++ Modules/dataManager.lua
@@ -0,0 +1,33 @@
+--// Services
+local HttpService = game:GetService("HttpService")
+
+--// Variables
+local FOLDER_NAME = "meteor_data"
+
+local saveData = function(fileName, data)
+    pcall(function()
+        task.spawn(function()
+            if isfolder(`{FOLDER_NAME}/`) then
+                writefile(`{FOLDER_NAME}/{fileName}.json`, HttpService:JSONEncode(data))
+            else
+                makefolder(`{FOLDER_NAME}/`)
+                writefile(`{FOLDER_NAME}/{fileName}.json`, HttpService:JSONEncode(data))
+            end
+        end)
+    end)
+end
+
+local loadData = function(fileName)
+    pcall(function()
+        if isfolder(`{FOLDER_NAME}/`) then
+           return readfile(`{FOLDER_NAME}/{fileName}.json`) 
+        end
+        return
+    end)
+end
+
+return {
+    saveData = saveData,
+    loadData = loadData
+}
+
