16a237697a93f8bbcec36b019d2e376d671ed255
Evan <evan.carlin.dugard@gmail.com>
2024-09-11 00:05:06 +0200

    Correction et amélioration du data manager #2

--- Modules/dataManager.lua
+++ Modules/dataManager.lua
@@ -4,25 +4,27 @@ local HttpService = game:GetService("HttpService")
 --// Variables
 local FOLDER_NAME = "meteor_data"
 
-local saveData = function(fileName, data)
-    pcall(function()
-        task.spawn(function()
-            if isfolder(`{FOLDER_NAME}/`) then
-                writefile(`{FOLDER_NAME}/{fileName}.json`, HttpService:JSONEncode(data))
-            else
-                makefolder(`{FOLDER_NAME}/`)
-                writefile(`{FOLDER_NAME}/{fileName}.json`, HttpService:JSONEncode(data))
-            end
-        end)
-    end)
-end
-
 local loadData = function(fileName)
     pcall(function()
         if isfolder(`{FOLDER_NAME}/`) then
            return HttpService:JSONDecode(readfile(`{FOLDER_NAME}/{fileName}.json`)) 
         end
         return
+    end)
+end
+
+local saveData = function(fileName, data)
+    pcall(function()
+        task.spawn(function()
+            local previousData = loadData(fileName)
+            table.insert(previousData, data)
+            if isfolder(`{FOLDER_NAME}/`) then
+                writefile(`{FOLDER_NAME}/{fileName}.json`, HttpService:JSONEncode(previousData))
+            else
+                makefolder(`{FOLDER_NAME}/`)
+                writefile(`{FOLDER_NAME}/{fileName}.json`, HttpService:JSONEncode(previousData))
+            end
+        end)
     end)
 end
 
