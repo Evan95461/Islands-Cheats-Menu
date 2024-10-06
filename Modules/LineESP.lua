bf2f1d3984a4bb612cc69371dbbaaae27c702c6f
Evan <evan.carlin.dugard@gmail.com>
2024-10-06 17:35:24 +0200

    ajout du LineESP

---
+++ Modules/LineESP.lua
@@ -0,0 +1,73 @@
+local espObject = {}
+espObject.__index = espObject
+
+--// Services
+local Workspace = game:GetService("Workspace")
+local RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
+
+--// Variables
+local client = Players.LocalPlayer
+local camera = Workspace.CurrentCamera
+
+function new(color, thickness, transparency)
+
+    local espAttributs = {
+        tracer = Drawing.new("Line"),
+        visible = false,
+        color = color or Color3.fromRGB(255, 255, 255),
+        thickness = thickness or 1,
+        transparency = transparency or 1,
+    }
+    setmetatable(espObject, espAttributs)
+
+    return espAttributs
+
+end
+
+export type espObject = typeof(new(...))
+
+function espObject.Create(self: espObject, target: Instance)
+    
+    local Tracer = self.tracer
+
+    local function lineesp()
+        RunService.RenderStepped:Connect(function()
+            if target ~= nil and typeof(target) == "Instance" then
+                local targetVector, targetOnScreen
+                local playerVector, playerOnScreen
+
+                Tracer.Visible = self.visible
+                Tracer.Color = self.color
+                Tracer.Thickness = self.thickness
+                Tracer.Transparency = self.transparency
+
+                if target:FindFirstChild("Humanoid") and target:FindFirstChild("HumanoidRootPart") then
+                    targetVector, targetOnScreen = camera:WorldToViewportPoint(target.HumanoidRootPart.Position)
+                    playerVector, playerOnScreen = camera:WorldToViewportPoint(client.Character.PrimaryPart.Position)
+                end
+
+                if targetOnScreen and playerOnScreen then
+                    Tracer.From = Vector2.new(playerVector.X, playerVector.Y)
+                    Tracer.To = Vector2.new(targetVector.X, targetVector.Y)
+                    self.Visible = true
+                elseif not targetOnScreen or not playerOnScreen then
+                    self.Visible = false
+                end
+            end
+        end)
+    end
+    coroutine.wrap(lineesp)()
+end
+
+function espObject.UpdateVisible(self: espObject, visible: boolean)
+    self.visible = visible
+end
+
+function espObject.UpdateColor(self: espObject, color: Color3)
+    self.color = color
+end
+
+return {
+    new = new,
+}
